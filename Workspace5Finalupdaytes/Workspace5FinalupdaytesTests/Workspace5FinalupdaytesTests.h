7ee7e765760d7b98123610d8aafc56ca64c1b3b7
Robert DeNicola <thedizzle23@gmail.com>
2012-05-16 14:58:31 -0400

    alerts tables and other tests last minute before expungement and subsequent distibution

---
+++ Workspace5Finalupdaytes/Workspace5FinalupdaytesTests/Workspace5FinalupdaytesTests.h
@@ -0,0 +1,13 @@
+//
+//  Workspace5FinalupdaytesTests.h
+//  Workspace5FinalupdaytesTests
+//
+//  Created by Robert DeNicola on 5/16/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Workspace5FinalupdaytesTests : SenTestCase
+
+@end
---
+++ Workspace5Finalupdaytes/Workspace5FinalupdaytesTests/Workspace5FinalupdaytesTests.m
@@ -0,0 +1,32 @@
+//
+//  Workspace5FinalupdaytesTests.m
+//  Workspace5FinalupdaytesTests
+//
+//  Created by Robert DeNicola on 5/16/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "Workspace5FinalupdaytesTests.h"
+
+@implementation Workspace5FinalupdaytesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Workspace5FinalupdaytesTests");
+}
+
+@end
